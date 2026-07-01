15dc5bc4220ba033a5697f37ace7b0fd741c3bbb
Sanika Tare <sanikatare.work@gmail.com>
2026-07-01 11:36:12 +0530

    feat: add UI components index

---
+++ src/components/ui/index.ts
@@ -0,0 +1,16 @@
+/**
+ * UI Component Library Index
+ * Centralized exports for all primitive components
+ */
+
+export { Button } from "./Button";
+export type { } from "./Button";
+
+export { Card } from "./Card";
+export type { } from "./Card";
+
+export { Input } from "./Input";
+export type { } from "./Input";
+
+export { Badge } from "./Badge";
+export type { } from "./Badge";
